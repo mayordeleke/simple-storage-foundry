ca90b897251d5e12235b6a72b95b5e47b047e610
Mayordeleke <mayordeleke@gmail.com>
2026-06-05 16:09:48 +0000

    edited test file

--- test/SimpleStorage.t.sol
+++ test/SimpleStorage.t.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.20;
 
 import {Test} from "forge-std/Test.sol";
-import {SimpleStorage} from "../src/SimpleStorage.sol";
+import {SimpleStorage} from "../src/SimpleSrorage.sol";
 
 contract SimpleStorageTest is Test {
         SimpleStorage storageContract;
