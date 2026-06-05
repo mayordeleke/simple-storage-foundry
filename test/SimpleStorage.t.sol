d1fe50d09970337009ac9d51f10306c1dfc59864
Mayordeleke <mayordeleke@gmail.com>
2026-06-05 16:03:00 +0000

    added test file

---
+++ test/SimpleStorage.t.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+import {Test} from "forge-std/Test.sol";
+import {SimpleStorage} from "../src/SimpleStorage.sol";
+
+contract SimpleStorageTest is Test {
+        SimpleStorage storageContract;
+
+            function setUp() public {
+                        storageContract = new SimpleStorage();
+            }
+
+                function testInitialValueIsZero() public {
+                            uint256 value = storageContract.get();
+                                    assertEq(value, 0);
+                }
+
+                    function testSetValue() public {
+                                storageContract.set(100);
+                                        assertEq(storageContract.get(), 100);
+                    }
+}
+                    
+                
+            
